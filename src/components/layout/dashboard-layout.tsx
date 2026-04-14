fe53172d8efda7195ed3d68db4cb12b55f0abd4d
evilboss <jr.evilboss@gmail.com>
2026-04-14 19:43:13 +0800

    add dashboard layout component with sidebar and header

---
+++ src/components/layout/dashboard-layout.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Sidebar from './sidebar';
+import Header from './header';
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col ml-64 overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-y-auto p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
